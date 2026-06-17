018a8b049d4e3bbcaf73a2d274d227f9492cf54e
danya <etherdate@gmail.com>
2026-06-17 11:39:30 +0300

    feat: add csv reading to storage

--- storage.go
+++ storage.go
@@ -1,6 +1,12 @@
 package main
 
-import "os"
+import (
+	"encoding/csv"
+	"io"
+	"os"
+	"strconv"
+	"time"
+)
 
 type Storage struct {
 	file *os.File
@@ -16,4 +22,46 @@ func NewStorage(filename string) (*Storage, error) {
 
 func (s *Storage) Close() error {
 	return s.file.Close()
+}
+
+func (s *Storage) ReadAll() ([]Expense, error) {
+	_, err := s.file.Seek(0, io.SeekStart)
+	if err != nil {
+		return nil, err
+	}
+
+	r := csv.NewReader(s.file)
+	r.FieldsPerRecord = 4
+	var res []Expense
+	for {
+		record, err := r.Read()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+
+		id, err := strconv.Atoi(record[0])
+		if err != nil {
+			return nil, err
+		}
+		desc := record[1]
+		amount, err := strconv.Atoi(record[2])
+		if err != nil {
+			return nil, err
+		}
+		date, err := time.Parse(time.RFC3339, record[3])
+		if err != nil {
+			return nil, err
+		}
+
+		res = append(res, Expense{
+			ID:     id,
+			Desc:   desc,
+			Amount: amount,
+			Date:   date,
+		})
+	}
+	return res, nil
 }
